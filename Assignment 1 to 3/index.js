e95217b6b0f52734446aa33913527a5ba2d5fd15
Saad_Naseem <167574691+saad-rao@users.noreply.github.com>
2024-08-16 19:38:14 +0500

    Add files via upload

---
+++ Assignment 1 to 3/index.js
@@ -0,0 +1,22 @@
+
+                    //   ALERT
+
+
+// var name1=('my name is saad naseem')
+// alert(name1)
+
+
+
+                      //    VARIABLE FOR STRING
+
+// var myName = ('My name is Saad naseem')
+// document.write(myName);
+
+
+
+                       // variable for number
+
+
+ var num1=10
+ var num2=10
+ document.write(num1+num2)                       
